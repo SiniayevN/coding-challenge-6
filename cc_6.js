521edb6ac7d0c64db27e07f3cd96005f28b2c1ac
Nikita Siniayev <ns21@usf.edu>
2025-10-05 14:24:25 -0400

    done?

--- cc_6.js
+++ cc_6.js
@@ -31,3 +31,32 @@ const emp3 = new Employee("Sophia", "Human Resources");
 
 const mgr1 = new Manager("Artem", "Finance", 4);
 const mgr2 = new Manager("Elena", "Sales", 6);
+
+//step 4 done
+
+class Company {
+  constructor() {
+    this.employees = [];
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
+  listEmployees() {
+    console.log("Company Employee List:");
+    this.employees.forEach(emp => {
+      console.log(emp.describe());
+    });
+  }
+}
+
+const company = new Company();
+company.addEmployee(emp1);
+company.addEmployee(emp2);
+company.addEmployee(mgr1);
+company.addEmployee(mgr2);
+company.listEmployees();
+ 
+//step 6 done??
+
